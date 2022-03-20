2ee8d7d5ad5f3c5b46df6861ce5be1096c11d004
Pametan <31518701+Pametan@users.noreply.github.com>
2022-03-20 17:52:33 +0100

    trying to fix bugs again

--- webpack.config.js
+++ webpack.config.js
@@ -35,16 +35,17 @@ module.exports = {
     path: path.resolve(__dirname, "build/"),
     filename: "bundle.js"
   },
-  devServer: {
+  /*devServer: {
     static: {
       directory: path.join(__dirname, "public/"),
     },
     port: 3000,
     hot: true
-  },
+  },*/
   plugins: [
     new HTMLWebpackPlugin({
-      template: "./public/index.html"
+      inject: true,
+      template: path.resolve("./public/index.html"),
     }),
   ]
 }
